f753005a7b0e2251292c7a6f968a12ce60024e96
RutujaPatil18 <prutuja189@gmail.com>
2022-10-04 15:26:32 +0530

    Modify:Chande in test case

--- src/test/java/com/MoodAnalyzer/AppTest.java
+++ src/test/java/com/MoodAnalyzer/AppTest.java
@@ -9,11 +9,19 @@ import org.junit.rules.ExpectedException;
  * Unit test for simple App.
  */
 public class AppTest {
-	/*
-	 * @Test public void CheckHappyMood() { MoodAnalyzer userObject = new
-	 * MoodAnalyzer("I am in Sad mood"); assertEquals("Sad",
-	 * userObject.analyseMood()); }
-	 */
+
+	@Test
+	public void CheckHappyMood() {
+		MoodAnalyzer userObject = new MoodAnalyzer("I am in Sad mood");
+		try {
+			ExpectedException exceptionRule = ExpectedException.none();
+			exceptionRule.expect(MoodAnalysisException.class);
+
+			assertEquals("Sad", userObject.analyseMood());
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	public void CheckForAnyMood() {
@@ -33,20 +41,21 @@ public class AppTest {
 		try {
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(MoodAnalysisException.class);
-			assertEquals("Happy",userObject.analyseMood());
+			assertEquals("Happy", userObject.analyseMood());
 		} catch (MoodAnalysisException e) {
-			assertEquals(MoodAnalysisException.ExcpetionType.NULL_INPUT,e.excpetionType);
+			assertEquals(MoodAnalysisException.ExcpetionType.NULL_INPUT, e.excpetionType);
 		}
 	}
+
 	@Test
 	public void CheckMoodForEmpty() {
 		MoodAnalyzer userObject = new MoodAnalyzer(null);
 		try {
 			ExpectedException exceptionRule = ExpectedException.none();
 			exceptionRule.expect(MoodAnalysisException.class);
-			assertEquals("Happy",userObject.analyseMood());
+			assertEquals("Happy", userObject.analyseMood());
 		} catch (MoodAnalysisException e) {
-			assertEquals(MoodAnalysisException.ExcpetionType.EMPTY_INPUT,e.excpetionType);
+			assertEquals(MoodAnalysisException.ExcpetionType.EMPTY_INPUT, e.excpetionType);
 		}
 	}
 
